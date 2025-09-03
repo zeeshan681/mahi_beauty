9f3b45752eb5e452adddc254296bc63da26f054e
zeeshan681 <zeeshanzeeshan11009900@gmail.com>
2025-09-04 00:54:11 +0500

    done with splah screen

--- lib/Views/00_Splash_Screen/splash_screen.dart
+++ lib/Views/00_Splash_Screen/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mahi_beauty/Core/coustm_colors.dart';
+import 'package:mahi_beauty/Views/01_Onboarding_Screens/onboarding_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,12 +9,48 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+
+    _controller.forward();
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => OnboardingScreen()),
+        );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: SplashscreenBackgroundcolor,
-      body: Column(children: [Image.asset("assets/images/Logo.png")]),
+      body: Center(
+        child: FadeTransition(
+          opacity: _animation,
+          child: Image.asset("assets/images/Logo.png"),
+        ),
+      ),
     );
   }
 }
